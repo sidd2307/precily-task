dfa5b6ec62a1c4b048562487e077c11e0a121e04
Siddheshwar Panda <siddheshwar.2307@gmail.com>
2021-10-05 08:16:37 +0530

    first commit

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
-// imports
-import express from "express";
-import mongoose from "mongoose";
-import route from "./route/routes.js";
-import cors from "cors";
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import Connection from "./db.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-// initialisations
-dotenv.config();
-const app = express();
+import Routes from './route/routes.js';
 
-app.use(bodyParser.json({ extended: true }));
+const app = express(); 
+
+app.use(bodyParser.json({extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors()); // Use this after the variable declaration
+app.use(cors());
 
-app.use("/users", route);
+app.use('/users', Routes);
 
-// port and mongo connection
-const PORT = process.env.PORT || 8000;
-const URL =
-  "mongodb+srv://admin:admin@cluster0.fax9n.mongodb.net/precilytask_1?retryWrites=true&w=majority";
+// https://www.mongodb.com/cloud/atlas
+// const USERNAME = process.env.USERNAME;
+// const PASSWORD = process.env.PASSWORD;
+// const URL = 'mongodb+srv://user:Manrun89@cluster0.zgrax.mongodb.net/CRUDAPP?retryWrites=true&w=majority';
+// const URL = 'mongodb://user:Manrun89@cluster-first-shard-00-00.zgrax.mongodb.net:27017,cluster-first-shard-00-01.zgrax.mongodb.net:27017,cluster-first-shard-00-02.zgrax.mongodb.net:27017/CRUDAPP?ssl=true&replicaSet=atlas-dair4b-shard-0&authSource=admin&retryWrites=true&w=majority'
+// const URL = 'mongodb://user:codeforinterview@crudapp-shard-00-00.zgrax.mongodb.net:27017,crudapp-shard-00-01.zgrax.mongodb.net:27017,crudapp-shard-00-02.zgrax.mongodb.net:27017/CRUDAPP?ssl=true&replicaSet=atlas-as0xva-shard-0&authSource=admin&retryWrites=true&w=majority'
+const URL = 'mongodb+srv://admin:admin@cluster0.fax9n.mongodb.net/precilytask_1?retryWrites=true&w=majority'
 
-Connection(process.env.MONGODB_URI || URL);
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
-}
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { 
 
-app.listen(PORT, () =>
-  console.log(`Server is running successfully on PORT ${PORT}`)
-);
+    app.listen(8000, () => console.log(`Server is running on PORT: 8000`))
+}).catch((error) => {
+    console.log('Error:', error.message)
+})
